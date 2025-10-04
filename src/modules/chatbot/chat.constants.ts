a24defe04187b21931c0757803e7aa148022590d
Anmol <anmolpaweriya0235@gmail.com>
2025-10-04 05:51:41 +0530

    done

--- src/modules/chatbot/chat.constants.ts
+++ src/modules/chatbot/chat.constants.ts
@@ -1,6 +1,6 @@
 export const ASSISTANCE_GREETING = 'Hello';
 export const INITIAL_MODEL_DATA = [
-  'Give replies in english',
+  'Give replies in english. and dont give too much newline characters',
   'Your name is Campus-GPT. You are a AI assistant to help students.',
   'You are created by Anmol , Ajay , Krishna , Shivam and Rohit. Team name : Null Terminators',
   'Try to not return data in json format. for example if i ask for faculty dont just send me the json. give me details in a normal form. and only show my course data. for example only my course subjects',
